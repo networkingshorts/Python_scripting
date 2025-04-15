f46edfe765bf521728c78a33d2d580513e964a3f
networkingshorts <tauheedshkectc200@gmail.com>
2025-04-15 22:58:10 +0530

    Created Function Python Script for Cisco and Juniper Routers

---
+++ python_scripting_using_function.py
@@ -0,0 +1,59 @@
+from paramiko import client
+import time
+
+ssh_client = client.SSHClient()
+
+ssh_client.set_missing_host_key_policy(client.AutoAddPolicy())
+
+juniper_cmd = ['conf t', 'router ospf 1', 'commit','end']
+cisco_cmd = ['show ip interface brief','conf t','int g0/0/0/0','ip address 100.100.100.1 255.255.255.255','no shut','end']
+
+def cisco_ios_xr():
+    username = 'admin'
+    hostname = 'sandbox-iosxr-1.cisco.com'
+    password = 'C1sco12345'
+
+    ssh_client.connect(hostname = hostname, username = username, password = password, port = 22, look_for_keys= False)
+    print(f"Device is successfully connected to {hostname}")
+    device_access = ssh_client.invoke_shell()
+    device_access.send("terminal len 0 \n")
+    for cmd in cisco_cmd:
+
+        device_access.send(f"{cmd}\n")
+        time.sleep(2)
+        output = device_access.recv(65535)
+        print(output.decode())
+    device_access.send("show ip interface brief")
+    time.sleep(5)
+    output = device_access.recv(65535)
+    print(output.decode(),end="")
+    ssh_client.close()
+
+
+cisco_ios_xr()
+
+def juniper():
+    username = 'admin'
+    hostname = 'sandbox-iosxr-1.cisco.com'
+    password = 'C1sco12345'
+
+    ssh_client.connect(hostname = hostname, username = username, password = password, port = 22, look_for_keys= False)
+    print(f"Device is successfully connected to Juniper")
+    device_access = ssh_client.invoke_shell()
+    device_access.send("terminal len 0 \n")
+    for cmd in juniper_cmd:
+
+        device_access.send(f"{cmd}\n")
+        time.sleep(1)
+        output = device_access.recv(65535)
+        print(output.decode(), end='')
+    device_access.send("show running-config router ospf")
+    time.sleep(5)
+    output = device_access.recv(65535)
+    print(output.decode(),end="")
+    ssh_client.close()
+
+juniper()
+
+
+
