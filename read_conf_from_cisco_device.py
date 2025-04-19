47690437fc03afac9ed5bfc97eb864bf45d982e9
networkingshorts <tauheedshkectc200@gmail.com>
2025-04-19 22:09:27 +0530

    Added read config from cisco device and write to cisco device in python script

---
+++ read_conf_from_cisco_device.py
@@ -0,0 +1,41 @@
+from paramiko import client, ssh_exception
+import time
+import os
+import socket
+username = 'admin'
+hostname = 'sandbox-iosxr-1.cisco.com'
+password = 'C1sco12345'
+
+
+print(f'Current Working Directory is: {os.getcwd()}')
+
+cisco_config = ["show run\n"]
+
+
+def device_cisco(hostname,cmd):
+    try:
+        ssh_client = client.SSHClient()
+
+        ssh_client.set_missing_host_key_policy(client.AutoAddPolicy())
+        ssh_client.connect(hostname=hostname,username=username,password=password,port=22,look_for_keys=False)
+
+
+        device_access = ssh_client.invoke_shell()
+        device_access.send('terminal len 0\n')
+        with open ('config.txt','w') as config_file:
+            for command in cmd:
+                device_access.send(f'{command}\n')
+                time.sleep(2)
+                output = device_access.recv(65535)
+                print(output.decode())
+                config_file.write(output.decode())
+    except ssh_exception.AuthenticationException:
+        print("Authentication Failed, Check Credentials")
+    except socket.gaierror:
+        print("Check the hostname")
+
+    
+    ssh_client.close()
+
+device_cisco('sandbox-iosxr-1.cisco.com',cisco_config)
+
